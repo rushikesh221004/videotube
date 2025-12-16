94f7dbd4b4e5f51936f68b7779c3f02901b0dfd1
Rushikesh Manjre <rushikeshmanjre221004@gmail.com>
2025-12-16 16:34:40 +0000

    connection successfull with database

---
+++ src/db/db.connect.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDb = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`db connected : ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log(`connection failed with db : ${error?.message}`);
+        process.exit();
+    }
+}
+
+export default connectDb;
