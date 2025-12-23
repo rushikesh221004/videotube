0b614ebcfb605b3aaaf568a1d35a194a4fd32392
rishi <rushikeshmanjre221004@gmail.com>
2025-12-23 06:30:46 +0000

    constants.js & db.connect.js this files are updated

---
+++ src/db/db.connect.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { dbName } from "../constants.js";
+
+dotenv.config();
+
+const connectDb = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${dbName}`);
+        console.log(`connected to db : ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log(`connection failed with mongoose : ${error?.message}`);
+        process.exit(1);
+    }
+}
+
+export default connectDb;
