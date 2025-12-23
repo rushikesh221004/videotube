5abb4f25425804e3298e6ef31798657d95519742
rishi <rushikeshmanjre221004@gmail.com>
2025-12-23 06:25:17 +0000

    app.js file updated

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.PORT,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser);
+
+export default app;
