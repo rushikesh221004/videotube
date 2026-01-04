989b3f9ff9acc9ac892c661ff879ec33a43a697e
rushikesh221004 <rushikeshmanjre221004@gmail.com>
2026-01-04 09:59:54 +0000

    app.js file updated

--- src/app.js
+++ src/app.js
@@ -7,15 +7,17 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.PORT,
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cookieParser);
-
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 export default app;
